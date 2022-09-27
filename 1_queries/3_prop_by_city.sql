facd706d9cf51415753e40e9f660026dbb373c78
jradigan17 <jradigan@hotmail.com>
2022-09-27 15:49:48 -0400

    add initial queries

---
+++ 1_queries/3_prop_by_city.sql
@@ -0,0 +1,11 @@
+-- Propery by city
+-- Show specific details about properties located in Vancouver including their average rating
+
+SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_id = properties.id
+WHERE city LIKE '%ancouv%'
+GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
+ORDER BY cost_per_night 
+LIMIT 10;
